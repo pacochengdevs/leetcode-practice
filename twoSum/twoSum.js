fb35d45322474f23a0fbc2b74d2c7ef7e9a07a1a
pacochengdevs <pacochengdevs@gmail.com>
2022-08-07 18:21:11 +0800

    Added hash method for 2 sum problem

---
+++ twoSum/twoSum.js
@@ -0,0 +1,50 @@
+const {performance} = require('perf_hooks');
+
+// brute force
+var twoSum_brute_force = (nums,target) => {
+    for(var i =0; i < nums.length; i++){
+        if (i==nums.length-1){
+            return ('No Solution');
+        }
+        for(var j =1; j < nums.length - i; j++){
+            if(nums[i]+nums[i+j]==target){
+                return [i,i+j];
+            }
+        }
+    }
+}
+
+//hash method
+var twoSum_hash_method = (nums,target) =>{
+    hash_map = {}
+    for(var i = 0; i <nums.length; i++){
+        diff = target - nums[i]
+        if(diff in hash_map){
+            return [hash_map[diff],i]
+        }
+        hash_map[nums[i]] = i
+    }
+}
+
+// test arrays
+test_nums = Array.from(Array(100001).keys())
+test_target = 199999
+
+
+// Performance for hash method
+var start = performance.now();
+
+output = twoSum_hash_method(test_nums,test_target)
+console.log(output)
+
+var end = performance.now();
+console.log(`Execution time: ${end - start} ms`);
+
+// Performance for brute force 
+var start_1 = performance.now();
+
+output = twoSum_brute_force(test_nums,test_target)
+console.log(output)
+
+var end_1 = performance.now();
+console.log(`Execution time: ${end_1 - start_1} ms`);
